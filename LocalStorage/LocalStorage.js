82101ab27081fbce9a229bf01d0a3747a0e487d4
johnson-rick <49804952+johnson-rick@users.noreply.github.com>
2019-06-07 17:39:25 -0400

    Week 07 changes

--- LocalStorage/LocalStorage.js
+++ LocalStorage/LocalStorage.js
@@ -2,24 +2,50 @@
     //localStorage.clear();
     var hasIsbns = checkForArray('isbns');
     
-    if (hasIsbns == true)
-    {
-        var isbns = getItemFromStorage('isbns');
-        if (isbns != null && isbns != undefined)
+    var hasIsbns = checkForArray('isbns');
+	var ddlIsbn = document.getElementById('ddlIsbn');
+	ddlIsbn.addEventListener(
+        'change',
+        function ()
         {
+            fadeInTextIsbn(this.value);
+        },
+        false
+    );
+	
+    if (hasIsbns == true) {
+        var isbns = getItemFromStorage('isbns');
+        if (isbns != null && isbns != undefined) {
+			isbns.sort(sortNumber); //build in numerical order
             for (var i = 0; i < isbns.length; i++) {
                 var isbn = isbns[i];
                 var book = [];
                 var item = getItemFromStorage(isbn);
 
-                if (item != null && book != undefined)
-                {
+                if (item != null && book != undefined) {
                     book.push(getItemFromStorage(isbn));
                     CreateTable(book);
+                    addItemToDropdown(isbn, isbn);
                 }
             }
         }
-        //console.log(isbns);
+		document.getElementById('ddlIsbn').classList.remove('hide', 'inline');
+		document.getElementById('ddlIsbn').classList.add('unhide', 'inline');
+    }
+	else
+        {
+			document.getElementById('ddlIsbn').classList.add('hide', 'inline');
+            document.getElementById('inputIsbn').classList.remove('hide', 'inline');
+            document.getElementById('inputIsbn').classList.add('unhide', 'inline');
+        }
+	addItemToDropdown("999999", "Add New");
+
+    function addItemToDropdown(value, display) {
+        var opt = document.createElement("option");
+        opt.value = 'opt_' + value;
+        opt.text = display.toString();     
+        opt.id = 'opt_' + value;
+		ddlIsbn.appendChild(opt);
     }
 
     function checkForArray(keyName)
@@ -97,8 +123,28 @@
         this.removeItem(arrayName);
     }
 
-    document.getElementById('btnSearch').addEventListener('click', validate, false)
-    document.getElementById('btnClear').addEventListener('click', clearTable, false)
+    document.getElementById('btnSearch').addEventListener('click', validate, false);
+    document.getElementById('btnClear').addEventListener('click', clearTable, false);
+	//apply event listener to all trash icons by class 'fa-trash'
+	var classname = document.getElementsByClassName("fa-trash");
+	for (var i = 0; i < classname.length; i++) {
+		classname[i].addEventListener(
+			'click', 
+			function ()
+			{
+				var self = this;
+				removeIsbn(self);
+			},
+       false
+    )};
+	document.getElementById('ddlStyle').addEventListener(
+        'change',
+        function ()
+        {
+            changeStyle(this.value);
+        },
+        false
+    );
 
     class Book {
         constructor(title, publisher, published, pages, isbns, description, genre, link, author) {
@@ -158,6 +204,7 @@
                     books.push(book);
                     var isbn = document.getElementById('isbn').value;
                     setItemInStorage(isbn, book);
+					insertInOrder(isbn);
                 }
             }               
         });
@@ -182,11 +229,11 @@
             arr.forEach(function (item) {
                 if (item.type == "ISBN_13") {
                     isbn_13 = item.identifier;
-                    insertIsbn(item.identifier)
+                    insertIsbn(item.identifier);
                 }
                 else if (item.type == "ISBN_10") {
                     isbn_10 = item.identifier;
-                    insertIsbn(item.identifier)
+                    insertIsbn(item.identifier);
                 }
             });
 
@@ -224,6 +271,61 @@
         } 
     }
 
+	function sortNumber(a, b) {
+		return a - b;
+	}
+
+	function insertInOrder(newIsbn){
+		 var isbns = getItemFromStorage('isbns');
+		 isbns.sort(sortNumber); //sort array numerically
+		 var nextIsbn = BigInt(-1);
+		 
+		 //find first isbn number that exceeds new isbn
+		for (var i = 0; i < isbns.length; i++) { 
+			if(BigInt(isbns[i]) > BigInt(newIsbn)){
+				nextIsbn = isbns[i];
+			}
+		}	
+		
+		var ddl = document.getElementById("ddlIsbn");
+		ddl.classList.remove('hide');
+		ddl.classList.add('unhide');
+		
+		var opt = document.createElement("option");
+		var nodeToInsertBefore;
+		if(nextIsbn == -1){
+			nodeToInsertBefore = document.getElementById('opt_999999'); //insert before Add New
+		}
+		else {
+			nodeToInsertBefore = document.getElementById('opt_' + nextIsbn);
+		}
+		opt.value = "opt_" + newIsbn
+		opt.text = newIsbn;     
+		opt.id = 'opt_' + newIsbn;
+		
+		//check if already exists in select (only insert if not already exists)
+		if(!optionExists( 'opt_' + newIsbn, ddl)){
+			ddl.insertBefore(opt, nodeToInsertBefore);
+		}	 
+	}
+
+	function optionExists ( opt, sel )
+	{
+		var optionExists = false,
+			optionsLength = sel.length;
+
+		while ( optionsLength-- )
+		{
+			if ( sel.options[ optionsLength ].value === opt )
+			{
+				optionExists = true;
+				break;
+			}
+		}
+		return optionExists;
+	}
+
+
     function CheckIfDuplicate(book, books) {
         var count = 0;
 
@@ -235,6 +337,20 @@
 
         return Boolean(count);
     }
+	
+	function removeIsbn(ref){
+		var rowId = ref.parentNode.parentNode.id;
+		var isbn = rowId.substr(4);
+		var rowIdx = document.getElementById(rowId).rowIndex;
+		//remove child from select
+		var ddl = document.getElementById('ddlIsbn');
+		var opt = document.getElementById('opt_' + isbn);
+		ddl.removeChild(opt);
+		//remove the row
+		document.getElementById('Books').deleteRow(rowIdx);
+		//array isbn
+		//TO DO: delete the related isbn from the isbns array in localstorage
+	}
 
     function CreateTable(books) {
         var dataitems = [];
@@ -248,7 +364,8 @@
                       item.description,
                       item.genre,
                       '<a href="' + item.link + '" target="_blank">View</a>',
-                      item.author);
+                      item.author,
+					  '<i class="fa fa-trash"></i>');
             dataitems.push(data);
         });
 
@@ -256,18 +373,28 @@
         if (dataitems.length > 0)
         {
             var divTable = document.getElementById('divTable');
-            divTable.className = "visible";
+            divTable.className = "visible customStyle";
         }
 
         for (var i = 0; i < dataitems.length; i++) {
              //create a new row
             var newRow = table.insertRow(table.rows.length);
-
+			var isbns = dataitems[i][4];
+			var rowIds = isbns.split(",");
+			var rowId = rowIds[0];
+			newRow.id = "row_" + rowId;
+			newRow.className = "customStyle"
+			
             for (var j = 0; j < dataitems[i].length; j++) {
-                 //create a new cell
+				//create a new cell
                 var cell = newRow.insertCell(j);
+				cell.className = 'customStyle';
                  //add value to the cell
                 cell.innerHTML = dataitems[i][j];
+				//center trash icon
+				if(j == dataitems[i].length - 1){
+					cell.className = "customStyle center";
+				}
             }
         }
     }  
@@ -311,7 +438,7 @@
         var table = document.getElementById('Books').getElementsByTagName('tbody')[0];
         var divTable = document.getElementById('divTable');
         table.innerHTML = "";
-        divTable.className = "invisible";    
+        divTable.className = "invisible customStyle";    
         localStorage.clear();
         location.reload();
     }
@@ -326,5 +453,91 @@
                 break;
         }
     }
+
+    function fadeInTextIsbn(selectedValue)
+    {
+        if (selectedValue != null && selectedValue != undefined && selectedValue == 'opt_999999') {
+            document.getElementById('inputIsbn').classList.remove('hide', 'inline');
+            document.getElementById('inputIsbn').classList.add('unhide', 'inline');
+			//reset any previous highlighted rows
+			var rows = document.getElementsByClassName('highlightRow');
+			for (var i = 0; i < rows.length; i++) {
+				rows[i].classList.remove('highlightRow');
+			}
+        }
+        else
+        {
+            document.getElementById('inputIsbn').classList.remove('unhide', 'inline');
+            document.getElementById('inputIsbn').classList.add('hide', 'inline');
+			if(selectedValue != null && selectedValue != undefined && selectedValue != -1){
+				var isbn = selectedValue.substr(4);
+				var row = document.getElementById('row_' + isbn);
+				//first reset any previous highlighted rows
+				var rows = document.getElementsByClassName('highlightRow');
+				for (var i = 0; i < rows.length; i++) {
+					rows[i].classList.remove('highlightRow');
+				}
+				
+				row.classList.add('highlightRow');
+			}
+        }
+    }
+
+    var objSelect = document.getElementById("ddlIsbn");
+
+    //Set selected
+	if(objSelect != null){
+		setSelectedValue(objSelect, "--Select--");
+	}
+
+    function setSelectedValue(selectObj, valueToSet) {
+        for (var i = 0; i < selectObj.options.length; i++) {
+            if (selectObj.options[i].text == valueToSet) {
+                selectObj.options[i].selected = true;
+                return;
+            }
+        }
+    }
+	
+	function changeStyle(style){
+		switch(style) {
+				case 'style1':
+					document.getElementById('header').style.font = "55px 'Almendra SC'";
+					document.getElementById('header').style.color = "#43acb5";
+					document.getElementById('header').style.textShadow = "-2px 2px 0px #00e6e6, -4px 4px 0px #01cccc, -6px 6px 0px #00bdbd";
+					var rows = document.getElementsByClassName("customStyle"),
+						len = rows !== null ? rows.length : 0,
+						i = 0;
+					for(i; i < len; i++) {
+						rows[i].classList.add('style1'); 
+						rows[i].classList.remove('style2'); 
+					}
+					break;
+				case 'style2':
+					var rows = document.getElementsByClassName("customStyle"),
+						len = rows !== null ? rows.length : 0,
+						i = 0;
+					for(i; i < len; i++) {
+						rows[i].classList.remove('style1'); 
+						rows[i].classList.add('style2'); 
+					}
+					document.getElementById('header').style.font = "45px 'Bangers'";
+					document.getElementById('header').style.color = "#779bcd";
+					document.getElementById('header').style.textShadow = "6px 6px 0px rgba(0,0,0,0.2)";
+					break;
+				default:
+					document.getElementById('header').style.font = '';
+					document.getElementById('header').style.color = "#000000";
+					document.getElementById('header').style.textShadow = '';
+					var rows = document.getElementsByClassName("customStyle"),
+						len = rows !== null ? rows.length : 0,
+						i = 0;
+					for(i; i < len; i++) {
+						rows[i].classList.remove('style1'); 
+						rows[i].classList.remove('style2'); 
+					}
+		}
+	}
+
 });
 
