fc7e9ce1ad32102e6c3342a6f31955f77b1111b7
johnson-rick <49804952+johnson-rick@users.noreply.github.com>
2019-06-13 17:49:39 -0400

    Week 08 More changes

--- index.js
+++ index.js
@@ -36,11 +36,11 @@
     let s3 = new Score(3, 'JSON Parse, Stringify', '05/17/2019', '05/19/2019', 'Received a grade of Skilled', 0, '../Ajax/Ajax.html', '');
     let s4 = new Score(4, 'AJAX requesting a JSON file', '05/17/2019', '05/19/5019', 'Received a grade of Skilled', 0, '../Ajax/Ajax.html', '');
     let s5 = new Score(5, 'Local Storage API, Storing and Retrieving Simple Data, Arrays, Associative Arrays, and Objects', '05/24/2019', '05/27/2019', 'Received a grade of skilled', 0, '../LocalStorage/LocalStorage.html', '');
-    let s6 = new Score(6, 'DOM Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.', '06/07/2019', '', 'First attempt please review', 0, '../LocalStorage/LocalStorage.html', '');
-    let s7 = new Score(7, 'Manipulating CSS Class Properties Using JavaScript', '06/07/2019', '', 'First attempt please review', 0, '../LocalStorage/LocalStorage.html', '');
-    let s8 = new Score(8, 'Creating CSS3 Transitions and Animations in CSS and triggering them with JavaScript', '06/07/2019', '', 'First attempt please review', 0, '../LocalStorage/LocalStorage.html', '');
-    let s9 = new Score(9, 'Standard JavaScript Events Including those for Mobile Devices(Ex.onTouchBegin, onLoad, etc.) and Animation and Transition Events', '06/07/2019', '', 'First attempt please review', 0, '../LocalStorage/LocalStorage.html', '');
-    let s10 = new Score(10, 'HTML5 Tags - Video, Audio, and Canvas', '06/14/2019', '', 'Not submitted yet', 0, '../Javascript2/JavaScript2.html', '');
+    let s6 = new Score(6, 'DOM Manipulation Using createElement, appendChild, insertBefore, removeChild, etc.', '06/07/2019', '06/08/2019', 'Received a grade of skilled', 0, '../LocalStorage/LocalStorage.html', '');
+    let s7 = new Score(7, 'Manipulating CSS Class Properties Using JavaScript', '06/07/2019', '06/08/2019', 'Received a grade of skilled', 0, '../LocalStorage/LocalStorage.html', '');
+    let s8 = new Score(8, 'Creating CSS3 Transitions and Animations in CSS and triggering them with JavaScript', '06/13/2019', '', 'First attempt please review', 0, '../LocalStorage/LocalStorage.html', '');
+    let s9 = new Score(9, 'Standard JavaScript Events Including those for Mobile Devices(Ex.onTouchBegin, onLoad, etc.) and Animation and Transition Events', '06/13/2019', '', 'First attempt please review', 0, '../LocalStorage/LocalStorage.html', '');
+    let s10 = new Score(10, 'HTML5 Tags - Video, Audio, and Canvas', '06/13/2019', '', 'First attempt please review', 0, '../Html5Tags/html5.html', '');
     let s11 = new Score(11, 'Designing, Defining, and Triggering CSS3 Transitions without Custom Libraries', '06/14/2019', '', 'Not submitted yet', 0, '../Javascript2/JavaScript2.html', '');
     let s12 = new Score(12, 'Designing, Defining, and Triggering CSS3 Transforms without Custom Libraries', '06/21/2019', '', 'Not submitted yet', 0, '../Javascript2/JavaScript2.html', '');
     let s13 = new Score(13, 'Designing, Defining, and Triggering CSS3 Animations without Custom Libraries', '06/21/2019', '', 'Not submitted yet', 0, '../Javascript2/JavaScript2.html', '');
